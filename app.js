813f5817627f9b77b8512289a8b703e31e2f38ce
thehatwars <62159346+thehatwars@users.noreply.github.com>
2021-11-08 21:14:57 -0800

    updated opensea api updates

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
 //Init
 let ethAddress = "";
-let gifDiv ;
-let tbody ;
-let tValue ;
+let gifDiv;
+let tbody;
+let tValue;
 
 
-let makeRowEntry = (assImg, assName, assFloor, assCount) =>`<tr><th> <img height="50" src="${assImg}"/></th><th><a href="https://opensea.io/collection/${assName}?search[sortAscending]=true&search[sortBy]=PRICE&search[toggles][0]=BUY_NOW" target="_blank">${assName}</a></th><th>${assFloor} (${assCount})</th></tr>`;
+let makeRowEntry = (assImg, assName, assFloor, assCount) => `<tr><th> <img height="50" src="${assImg}"/></th><th><a href="https://opensea.io/collection/${assName}?search[sortAscending]=true&search[sortBy]=PRICE&search[toggles][0]=BUY_NOW" target="_blank">${assName}</a></th><th>${assFloor} (${assCount})</th></tr>`;
 
-function parseData(collecs){
-  let uiData = collecs.map(item => ({
-    collecName :item.slug, 
-    collecImg : item.image_url,
-    collecFlPrice : item.stats.floor_price,
-    collecOwnedCount : item.owned_asset_count
-  })).sort((a, b) => parseFloat(b.collecFlPrice) - parseFloat(a.collecFlPrice));
+function parseData(collecs) {
 
-  let tEValue = 0;
+  Promise.all(
+    collecs.map(element =>
+      fetch("https://api.opensea.io/api/v1/collection/" + element.slug + "/stats")
+      .then(response => response.json())
+      .catch(error => console.log('error', error))
+    )
+  ).then(data => {
+    let uiData = [];
+    for (i = 0; i < data.length; i++) {
+      let clitem = {};
+      clitem.collecName = collecs[i].slug;
+      clitem.collecImg = collecs[i].image_url;
+      clitem.collecFlPrice = data[i].stats.floor_price;
+      clitem.collecOwnedCount = collecs[i].owned_asset_count;
+      uiData.push(clitem);
+    }
 
-  let dataHtml =  `<tr><th>Image</th><th>Collection Name</th><th>ETH Floor Price (Count)</th></tr>`;
-  // add to html
-  uiData.forEach(colele => {
-    tEValue = tEValue + colele.collecFlPrice * colele.collecOwnedCount;
-    dataHtml = dataHtml + makeRowEntry(colele.collecImg,colele.collecName,colele.collecFlPrice, colele.collecOwnedCount);
+    uiData.sort((a, b) => parseFloat(b.collecFlPrice) - parseFloat(a.collecFlPrice));
+
+    let tEValue = 0;
+
+    let dataHtml = `<tr><th>Image</th><th>Collection Name</th><th>ETH Floor Price (Count)</th></tr>`;
+    // add to html
+    uiData.forEach(colele => {
+      tEValue = tEValue + colele.collecFlPrice * colele.collecOwnedCount;
+      dataHtml = dataHtml + makeRowEntry(colele.collecImg, colele.collecName, colele.collecFlPrice, colele.collecOwnedCount);
+    });
+
+    tValue.innerHTML = tEValue;
+    tbody.innerHTML = dataHtml;
+
   });
-  
-  tValue.innerHTML = tEValue;
-  tbody.innerHTML = dataHtml;
-
 }
 
 //get all assets from address
@@ -36,12 +50,12 @@ function get_floors() {
   ethAddress = document.getElementById("ethAddressInput").value;
 
   console.log("Getting assets for : " + ethAddress);
-  
+
   //add query to history
-  if(history.pushState){
-    let newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname + "?q=" + ethAddress ;
+  if (history.pushState) {
+    let newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname + "?q=" + ethAddress;
     window.history.pushState({
-      path : newUrl
+      path: newUrl
     }, '', newUrl);
   }
 
@@ -61,7 +75,7 @@ function get_floors() {
 
 }
 
-function setgif(url){
+function setgif(url) {
   gifDiv.innerHTML = `<img src="${url}"/>`;
 
 }
@@ -72,15 +86,15 @@ document.addEventListener("DOMContentLoaded", function () {
   tbody = document.getElementById("floortable");
   gifDiv = document.getElementById("gifs");
   tValue = document.getElementById("totalValue");
- 
-  document.body.addEventListener('keyup', function(event){
-    if(event.key === 'Enter'){
+
+  document.body.addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
       get_floors()
     }
   })
 
   //get url from window and search
-  if(location.search.split('q=')[1]){
+  if (location.search.split('q=')[1]) {
     document.getElementById("ethAddressInput").value = location.search.split('q=')[1];
     get_floors()
   }
@@ -91,8 +105,8 @@ document.addEventListener("DOMContentLoaded", function () {
       "Content-Type": "application/json"
     }
   };
-  let random = Math.floor(Math.random()*250)+1;
-  fetch("https://api.giphy.com/v1/gifs/search?q=Not gonna make it&offset="+ random +"&rating=pg&api_key=8jsnBo9dDCBW3d7lCTTKxCSmz7MMwtEy&limit=1", requestOptions)
+  let random = Math.floor(Math.random() * 250) + 1;
+  fetch("https://api.giphy.com/v1/gifs/search?q=Not gonna make it&offset=" + random + "&rating=pg&api_key=8jsnBo9dDCBW3d7lCTTKxCSmz7MMwtEy&limit=1", requestOptions)
     .then(response => response.json())
     .then(result => setgif(result.data[0].images.downsized_medium.url))
     .catch(error => console.log('error', error));
